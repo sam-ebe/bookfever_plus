0e0af2036c3bef7ea2cf44c12bfa53a0e125a320
Samuel Ebebenge <samuel.ebebenge@gmail.com>
2021-08-11 14:12:00 +0200

    Positioning elements in the Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { createStyles, alpha, makeStyles, Theme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
@@ -11,6 +11,11 @@ import FormGroup from '@material-ui/core/FormGroup';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import CustomDrawer from './CustomDrawer';
+import Badge from '@material-ui/core/Badge';
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import ConnectionForm from './ConnectionForm';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -22,6 +27,38 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     title: {
       flexGrow: 1,
+    },
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: alpha(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+      },
+      marginLeft: 0,
+      width: '100%',
+      marginRight: theme.spacing(2),
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(1),
+        width: 'auto',
+      },
+    },
+    inputRoot: {
+      color: 'inherit',
+
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(2)}px)`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '20ch',
+        '&:focus': {
+          width: '40ch',
+        },
+      },
     },
   }),
 );
@@ -54,13 +91,18 @@ export default function Navbar() {
       </FormGroup>
       <AppBar position="static">
         <Toolbar>
-            <CustomDrawer />
-
+          <CustomDrawer />
           <Typography variant="h6" className={classes.title}>
             Photos
           </Typography>
-          {auth && (
+
+          {(auth === true) ? 
             <div>
+              <IconButton aria-label="show 17 new notifications" color="inherit">
+                <Badge badgeContent={17} color="secondary">
+                  <NotificationsIcon />
+                </Badge>
+              </IconButton>
               <IconButton
                 aria-label="account of current user"
                 aria-controls="menu-appbar"
@@ -89,8 +131,23 @@ export default function Navbar() {
                 <MenuItem onClick={handleClose}>My account</MenuItem>
               </Menu>
             </div>
-          )}
+          : 
+          <ConnectionForm />}
+          
         </Toolbar>
+        <div className={classes.search}>
+          <InputBase
+            placeholder="Search…"
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}
+            inputProps={{ 'aria-label': 'search' }}
+          />
+          <IconButton type="submit"  aria-label="search validation">
+            <SearchIcon style={{ color: 'white' }} />
+          </IconButton>
+        </div>
       </AppBar>
     </div>
   );
