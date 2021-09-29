b2701bd354b725dbb4be06c5f4f7934c3b7db258
Samuel Ebebenge <samuel.ebebenge@gmail.com>
2021-09-29 22:58:56 +0200

    minimal version - initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Main from './components/Main';
-import { BrowserRouter as Router } from 'react-router-dom'
-
-// databooks needed from the beginning (in Categories button, Autocomplete search bar, Homepage)
-import {dataBooks} from './data/dataBooks';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import About from './pages/about';
+import Home from './pages/home';
+import Login from './components/Login';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <Header dataBooks={dataBooks} />
-        <Main dataBooks={dataBooks}/>
+      <div className="app">
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+            <Route path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/">
+            <Navbar />
+            <Home />
+          </Route>
+        </Switch>
       </div>
     </Router>
   );
---
+++ src/components/Title.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Title() {
+  return <div>My block title</div>;
+}
+
+export default Title;
