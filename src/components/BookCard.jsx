82b2af2eb4326e1d9114d7ad5247491f5e9ade9a
Samuel Ebebenge <samuel.ebebenge@gmail.com>
2021-10-05 22:07:41 +0200

    dialog categories

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -35,7 +35,7 @@ export default function BookCard(book) {
   const handleClose = () => {
     setOpen(false);
   };
-
+console.log(book.categories);
   return (
     <>
       <Card className='bookCard' sx={{ width: "260px", objectFit: "cover", margin: "15px 15px"}}>
@@ -92,6 +92,10 @@ export default function BookCard(book) {
                 <Typography color="textSecondary" component="p">
                   {/* Add quotations design */}
                   {"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 'de Finibus Bonorum et Malorum' (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum"}
+                </Typography>
+                <Typography className='categories' color="textSecondary" component="p">
+                  Categiories :
+                  <span className='categories__item'> {book.categories.join(', ')}</span>
                 </Typography>
               </DialogContent>
               <DialogActions>
